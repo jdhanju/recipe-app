353b5fd91bc08d9cb06835f9b0de93bfcb73b073
Jaiveer Dhanju <jaiveerdhanju1@gmail.com>
2023-06-03 12:28:56 -0700

    adding to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState, useEffect } from 'react';
+import RecipeForm from './RecipeForm';
+import RecipeList from './RecipeList';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [recipes, setRecipes] = useState(() => {
+    return JSON.parse(localStorage.getItem('recipes')) || [];
+  });
+  const [newRecipe, setNewRecipe] = useState({
+    name: '',
+    ingredients: '',
+    directions: ''
+  });
+
+  useEffect(() => {
+    const storedRecipes = localStorage.getItem('recipes');
+    if (storedRecipes) {
+      setRecipes(JSON.parse(storedRecipes));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('recipes', JSON.stringify(recipes));
+  }, [recipes]);
+
+  const handleInputChange = (e) => {
+    setNewRecipe({
+      ...newRecipe,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setRecipes([...recipes, newRecipe]);
+    setNewRecipe({
+      name: '',
+      ingredients: '',
+      directions: ''
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <RecipeForm
+        newRecipe={newRecipe}
+        handleInputChange={handleInputChange}
+        handleFormSubmit={handleFormSubmit}
+      />
+      <RecipeList recipes={recipes} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/RecipeForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const RecipeForm = ({ newRecipe, handleInputChange, handleFormSubmit }) => {
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <label className="formLabel">
+        Recipe Name:
+        <input
+          type="text"
+          name="name"
+          value={newRecipe.name}
+          onChange={handleInputChange}
+          required
+        />
+      </label>
+      <label className="formLabel">
+        Ingredients:
+        <textarea
+          name="ingredients"
+          value={newRecipe.ingredients}
+          onChange={handleInputChange}
+          required
+        />
+      </label>
+      <label className="formLabel">
+        Directions:
+        <textarea
+          name="directions"
+          value={newRecipe.directions}
+          onChange={handleInputChange}
+          required
+        />
+      </label>
+      <button type="submit">Add Recipe</button>
+    </form>
+  );
+};
+
+export default RecipeForm;
